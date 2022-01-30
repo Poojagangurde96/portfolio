a1fe34a0b228eb9f8257c50e4b5ce9513c457629
chetanj58 <chetanj58@gmail.com>
2022-01-30 14:50:54 +0530

    technologies added

--- src/helpers/Techs.js
+++ src/helpers/Techs.js
@@ -1,59 +1,27 @@
 
 export const Techs = [
     {
-        name: 'React',
-        iconName: 'react'
+        name: 'Redhat Linux',
+        iconName: 'redhat'
     },
     {
-        name: 'JavaScript',
-        iconName: 'javascript'
-    },
-    {
-        name: 'NodeJs',
-        iconName: 'nodejs'
-    },
-    {
-        name: 'HTML',
-        iconName: 'html5'
-    },
-    {
-        name: 'CSS',
-        iconName: 'css3'
-    },
-    {
-        name: 'SQL',
-        iconName: 'mysql'
-    },
-    {
-        name: 'JQuery',
-        iconName: 'jquery'
-    },
-    {
-        name: 'Bootstrap',
-        iconName: 'bootstrap'
-    },
-    {
-        name: 'Sass',
-        iconName: 'sass'
-    },
-    {
-        name: 'NPM',
-        iconName: 'npm'
-    },
-    {
-        name: 'Docker',
-        iconName: 'docker'
-    },
-    {
-        name: 'Git',
-        iconName: 'github'
+        name: 'Python',
+        iconName: 'python'
     },
     {
         name: 'Jira',
         iconName: 'jira'
     },
     {
-        name: 'Responsive',
-        iconName: 'responsive'
+        name: 'Cisco',
+        iconName: 'cisco'
+    },
+    {
+        name: 'MySql',
+        iconName: 'mysql'
+    },
+    {
+        name: 'AWS',
+        iconName: 'aws'
     }
 ]
